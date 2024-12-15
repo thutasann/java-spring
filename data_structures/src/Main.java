34c10599cffd020718d2b13ade838c68ac9259e5
thutasann <thutasann2002@gmail.com>
2024-12-16 00:23:24 +0800

    add: stack sample pls

---
+++ data_structures/src/Main.java
@@ -0,0 +1,10 @@
+import Stacks.StackSamples;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java Data structures and algorithms");
+
+        StackSamples stackSamples = new StackSamples();
+        stackSamples.SampleOne();
+    }
+}
---
+++ data_structures/src/Stacks/StackSamples.java
@@ -0,0 +1,32 @@
+package Stacks;
+import java.util.Stack;
+
+public class StackSamples {
+    /**
+     * Stack Sample One
+     * @apiNote - This is stack sample usage one
+     */
+    public void SampleOne() {
+        System.out.println("\n===> Hello From Stack Sample One");
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("Minecraft");
+        stack.push("Skyrim");
+        stack.push("DOOM");
+        stack.push("Borderlands");
+
+        String myFavGame = stack.pop();
+
+        System.out.println(stack);
+
+        System.out.println("My Fav Game --> " + myFavGame);
+
+        System.out.println(stack.peek());
+
+        System.out.println("Search value --> " + stack.search("Skyrim"));
+
+        for (int i = 0; i < 10000; i++) {
+            stack.push("Fallout76");
+        }
+    }
+}
