11ea3ffe2d227420dbe1471ebc62785ea462546f
thutasann <thutasann2002@gmail.com>
2024-12-19 22:39:24 +0800

    add: entities

---
+++ shopping_cart/src/main/java/com/thutasann/shopping_cart/model/Category.java
@@ -0,0 +1,33 @@
+package com.thutasann.shopping_cart.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+    public Category(String name) {
+        this.name = name;
+    }
+}
