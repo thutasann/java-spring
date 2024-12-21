2820f226e3bd339288248d66d769e403019fc3e2
thutasann <thutasann2002@gmail.com>
2024-12-21 21:41:08 +0800

    chore: add swagger

--- shopping_cart/README.md
+++ shopping_cart/README.md
@@ -1,1 +1,5 @@
 # Simple Spring Boot shopping cart backend
+
+## Swagger
+
+- http://localhost:9191/swagger-ui/index.html
--- shopping_cart/src/main/java/com/thutasann/shopping_cart/controller/ImageController.java
+++ shopping_cart/src/main/java/com/thutasann/shopping_cart/controller/ImageController.java
@@ -1,18 +1,27 @@
 package com.thutasann.shopping_cart.controller;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpHeaders;
 
 import com.thutasann.shopping_cart.dto.ImageDto;
+import com.thutasann.shopping_cart.model.Image;
 import com.thutasann.shopping_cart.response.ApiResponse;
 import com.thutasann.shopping_cart.service.image.IImageService;
+
+import io.swagger.v3.oas.annotations.Operation;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
@@ -25,6 +34,7 @@ public class ImageController {
     private final IImageService imageService;
 
     @GetMapping("/test")
+    @Operation(summary = "Image Endpoint Test", description = "This is the image endpoint test")
     public ResponseEntity<ApiResponse> imgTest() {
         return ResponseEntity.ok(new ApiResponse("Hello imgTest", "Hello test"));
     }
@@ -39,4 +49,15 @@ public class ImageController {
             return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Upload failed!", e.getMessage()));
         }
     }
+
+    @GetMapping("/image/download/{imageId}")
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
+        Image image = imageService.getImageById(imageId);
+        ByteArrayResource resource = new ByteArrayResource(
+                image.getImage().getBytes(1, (int) image.getImage().length()));
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"")
+                .body(resource);
+    }
+
 }
