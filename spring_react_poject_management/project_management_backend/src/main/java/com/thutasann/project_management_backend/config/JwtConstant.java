b0e8ce3b8c30878b535d090339fcd8b79552912b
thutasann <thutasann2002@gmail.com>
2025-01-04 19:28:24 +0800

    feat: JwtTokenValidator

---
+++ spring_react_poject_management/project_management_backend/src/main/java/com/thutasann/project_management_backend/config/JwtConstant.java
@@ -0,0 +1,16 @@
+package com.thutasann.project_management_backend.config;
+
+/**
+ * Jwt Constant
+ */
+public class JwtConstant {
+    /**
+     * JWT SECRET KEY
+     */
+    public static final String SECRET_KEY = "4xSm0H]d@Ojq%$ot.ey+$y+n|Fu8w|.=<+MD%v3&l_Z__#Ou%eJ%.[o <](F[n}f";
+
+    /**
+     * JWT HEADER
+     */
+    public static final String JWT_HEADER = "Authorization";
+}
