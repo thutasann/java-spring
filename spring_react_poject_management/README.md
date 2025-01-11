c6305784f63b65c48b7d4e7cfa4f6ce1a1c154e9
thutasann <thutasann2002@gmail.com>
2025-01-12 05:03:09 +0800

    feat: add RedisConfig

--- spring_react_poject_management/README.md
+++ spring_react_poject_management/README.md
@@ -1,5 +1,10 @@
 # Full Stack Project Management Using Spring Boot, React.
 
+- Spring boot
+- Redis
+- Spring Data JPA
+- MySql
+
 ## Swagger
 
 - http://localhost:8080/swagger-ui/index.html
---
+++ spring_react_poject_management/project_management_backend/src/main/java/com/thutasann/project_management_backend/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.thutasann.project_management_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    /**
+     * Redis Template
+     * 
+     * @param connectionFactory - Redis Connection Factory
+     * @return Redis Template
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
