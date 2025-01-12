468a9133c3a3b842a1e8be2d5ee727bc2006c756
thutasann <thutasann2002@gmail.com>
2025-01-12 20:42:15 +0800

    chore: trading backend setup

---
+++ spring_react_trading_platform/trading_backend/src/test/java/com/thuta/trading_backend/TradingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thuta.trading_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
