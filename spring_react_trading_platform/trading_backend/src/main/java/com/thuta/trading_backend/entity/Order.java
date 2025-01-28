37e7598652414a53bf12e85e55953de8111798dd
thutasann <thutasann2002@gmail.com>
2025-01-28 21:39:00 +0800

    fix: entity error

--- spring_react_trading_platform/trading_backend/src/main/java/com/thuta/trading_backend/entity/Order.java
+++ spring_react_trading_platform/trading_backend/src/main/java/com/thuta/trading_backend/entity/Order.java
@@ -14,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 /**
@@ -21,6 +22,7 @@ import lombok.Data;
  */
 @Entity
 @Data
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
