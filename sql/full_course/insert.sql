a824560ad4091fedd7dac0ce84f4f17c18094437
thutasann <thutasann2002@gmail.com>
2025-01-28 23:37:31 +0800

    add: insert

---
+++ sql/full_course/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO employees
+VALUES  (1, "thuta", "sann", "thutasann2002@gmail.com", 25.50, "2025-01-01"), 
+        (3, "hsu" , "yeywel" , "hsuyeywel@gmail.com", 25.50, "2025-01-01"),
+        (4, "kyaw" , "kyaw" , "kyaw@gmail.com", 25.50, "2025-01-01");
+
+SELECT * FROM employees;
+
+INSERT INTO employees (employee_id, first_name, last_name)
+VALUES (5, "aung", "aung");
