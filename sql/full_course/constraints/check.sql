f6668a89463874f9543c83b160a3b59c21e407aa
thutasann <thutasann2002@gmail.com>
2025-01-29 01:46:47 +0800

    refactor: categorize sqls

---
+++ sql/full_course/constraints/check.sql
@@ -0,0 +1,10 @@
+SELECT * FROM employees;
+
+ALTER TABLE employees
+ADD CONSTRAINT chk_hourly_pay CHECK(hourly_pay >= 10.00);
+
+INSERT INTO employees
+VALUES(7, "myo", "myo", "myo@gmail.com", 3, "2025-01-01");
+
+ALTER TABLE employees
+DROP CHECK chk_hourly_pay;
