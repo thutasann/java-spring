30455fe645a99b9db08c5dfcfcd4ee2c337b0578
thutasann <thutasann2002@gmail.com>
2025-01-29 16:37:15 +0800

    add: wild cards

---
+++ sql/full_course/clause/wild_cards.sql
@@ -0,0 +1,11 @@
+SELECT * FROM employees
+WHERE first_name LIKE "%h%"
+
+SELECT * FROM employees
+WHERE hire_date LIKE "2025%"
+
+SELECT * FROM employees
+WHERE job LIKE "_anage_"
+
+SELECT * FROM employees
+WHERE job LIKE "__o%"
