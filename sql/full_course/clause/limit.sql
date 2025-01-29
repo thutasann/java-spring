a5bb7d237cd10306d8238917a53873cdf0a80c57
thutasann <thutasann2002@gmail.com>
2025-01-29 16:52:10 +0800

    add: limit order by

---
+++ sql/full_course/clause/limit.sql
@@ -0,0 +1,8 @@
+SELECT * FROM customers
+LIMIT 2;
+
+SELECT * FROM customers
+ORDER BY last_name DESC LIMIT 3;
+
+SELECT * FROM customers
+LIMIT 2, 1;
---
+++ sql/full_course/clause/order_by.sql
@@ -0,0 +1,11 @@
+SELECT * FROM employees
+ORDER BY last_name ASC;
+
+SELECT * FROM employees
+ORDER BY first_name DESC;
+
+SELECT * FROM employees
+ORDER BY hire_date DESC;
+
+SELECT * FROM transactions
+ORDER BY amount ASC, customer_id DESC;
