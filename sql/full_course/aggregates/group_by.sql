00efd565cd38992056cf6b187cc3df3a6c349dad
thutasann <thutasann2002@gmail.com>
2025-01-31 23:47:12 +0800

    add: group by

--- sql/full_course/aggregates/group_by.sql
+++ sql/full_course/aggregates/group_by.sql
@@ -1,3 +1,19 @@
 -- GROUP BY = aggregate all rows by a specific column
 --            often used with aggregate functions
 --            ex. SUM(), MAX(), MIN(), AVG(), COUNT()
+
+SELECT * FROM transactions;
+
+ALTER TABLE transactions
+ADD COLUMN order_date DATETIME;
+
+--------
+
+SELECT MAX(amount), order_date
+FROM transactions
+GROUP BY order_date;
+
+SELECT COUNT(amount), customer_id
+FROM transactions
+GROUP BY customer_id
+HAVING COUNT(amount) > 1 AND customer_id IS NOT NULL;
