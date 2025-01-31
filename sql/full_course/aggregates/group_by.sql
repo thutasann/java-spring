1eb7eee05e747a9a2b33713f79ada9de7bcb76f1
thutasann <thutasann2002@gmail.com>
2025-01-31 23:14:03 +0800

    fix: gha deprecated

---
+++ sql/full_course/aggregates/group_by.sql
@@ -0,0 +1,3 @@
+-- GROUP BY = aggregate all rows by a specific column
+--            often used with aggregate functions
+--            ex. SUM(), MAX(), MIN(), AVG(), COUNT()
---
+++ sql/full_course/sub_queries/sub_queries.sql
@@ -0,0 +1,14 @@
+SELECT first_name, last_name, 
+       (SELECT AVG(hourly_pay) FROM employees) AS avg_pay 
+from employees;
+
+SELECT first_name, last_name, hourly_pay
+FROM employees
+WHERE hourly_pay > (SELECT AVG(hourly_pay) FROM employees)
+
+SELECT first_name, last_name
+FROM customers
+WHERE customer_id IN
+(SELECT DISTINCT customer_id
+FROM transactions
+WHERE customer_id IS NOT NULL);
